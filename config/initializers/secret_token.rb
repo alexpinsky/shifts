57248d6d9295afa7dcaa19ddb86584a5e35559a9
Alex Pinsky <alex_pinsky85@yahoo.com>
2013-11-16 21:39:39 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkPlace::Application.config.secret_token = '743c9f05f96e34cfd759601de89566aa15c86cf9c292f9a6a9f20768db67566d1f6595615929b90166cba70ed73bec3dbb2773f7057de1fe2f285ec346a1d571'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+WorkPlace::Application.routes.draw do
+  devise_for :users
+
+  root :to => "pages#home"
+end
