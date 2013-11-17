4b57a3e05988b54626ebe292592736efb738e1b1
Alex Pinsky <alex_pinsky85@yahoo.com>
2013-11-17 20:56:09 +0200

    completed shared login/home page

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.12'
 
 
 gem 'pg'
+gem 'hirb'
 gem 'devise'
 gem 'formtastic'
 gem 'formtastic-bootstrap'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
-	skip_before_filter :authenticate_user!
+	skip_before_filter :authenticate_user!, only: [:home]
 
  	def home
-   
  	end
 end
